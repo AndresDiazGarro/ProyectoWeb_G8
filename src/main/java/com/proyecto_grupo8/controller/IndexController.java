033b87e0e7f38d4fdb228959242d6f8011b2e855
AndresDiazGarro <iandresfy@gmail.com>
2022-06-29 21:20:40 -0600

    Prueba1

---
+++ src/main/java/com/proyecto_grupo8/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.proyecto_grupo8.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@Slf4j
+public class IndexController {
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        log.info("Ahora utilizamos MVC");
+        
+        return "index";
+    }
+}
---
+++ src/test/java/com/proyecto_grupo8/ProyectoGrupo8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_grupo8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
