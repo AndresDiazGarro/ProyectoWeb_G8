94fd8f100184567e6947aff43c4547ebf647169d
josepablo27 <jduran60412@ufide.ac.cr>
2022-07-08 01:50:35 -0600

    commit ubicacion

---
+++ src/main/java/com/proyecto_grupo8/controller/UbicacionController.java
@@ -0,0 +1,16 @@
+package com.proyecto_grupo8.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@Slf4j
+public class UbicacionController {
+    
+    @GetMapping("/ubicacion")
+    public String abrirUbicacion(){
+        return "ubicacion";
+    }
+}
