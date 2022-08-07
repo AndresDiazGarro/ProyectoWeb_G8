d71c6c009d19f6b9ba41ae082c4433330fe438c0
AndresDiazGarro <iandresfy@gmail.com>
2022-08-06 22:26:10 -0600

    Implementando Bootstrap

--- src/main/java/com/proyecto_grupo8/controller/IndexController.java
+++ src/main/java/com/proyecto_grupo8/controller/IndexController.java
@@ -1,13 +1,9 @@
 package com.proyecto_grupo8.controller;
 
-import com.proyecto_grupo8.domain.Factura;
-import com.proyecto_grupo8.domain.Producto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.proyecto_grupo8.service.FacturaService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 @Slf4j
